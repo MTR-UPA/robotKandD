a15f135da7978042709625f0778979efcab6a7f5
Luis Anaya <luis.ernesto.anaya@upa.edu.mx>
2026-09-12 12:05:55 -0600

    Figura 1

--- 01 Spatial Transformations/01 Homogeneus Transform Matrix/Figura1.m
+++ 01 Spatial Transformations/01 Homogeneus Transform Matrix/Figura1.m
@@ -14,13 +14,18 @@ color = [0.5 0.6 0.4];
 %Construye los marcos de referencia
 T0 = eye(4);
 T1 = T0*transl(0,a,f)*trotx(90)*trotz(180);
+T2 = T1*troty(-90)*trotx(90)*transl(0,-b,-(a-d));
 
 %Construye los vértices (son marcos de referencia sin dibujar)
 V1 = T0*transl(0,c+e,0);
+V2 = T1*transl(b,0,0);
 
 %Dibujo los marcos de referencia
 trplot(T0, 'frame', '0');
 trplot(T1, 'frame', '1');
+trplot(T2, 'frame', '2');
 
 %Dibulo las líneas
 dibujarLinea(T0, V1,"color",color)
+dibujarLinea(T1, V1,"color",color)
+dibujarLinea(T1, V2,"color",color)
